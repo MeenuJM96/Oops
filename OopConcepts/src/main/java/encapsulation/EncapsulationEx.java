8ea9799bf45b6234f5ab6702061cd659f0819f00
hp <hp@LAPTOP-116NDJE9>
2024-04-23 21:50:53 +0530

    Encapsulation

---
+++ OopConcepts/src/main/java/encapsulation/EncapsulationEx.java
@@ -0,0 +1,11 @@
+package encapsulation;
+
+public class EncapsulationEx {
+	private String name;
+	public void getName() {
+		System.out.println("Name:"+name);
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+}
---
+++ OopConcepts/src/main/java/encapsulation/EncapsulationEx1.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class EncapsulationEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EncapsulationEx obj=new EncapsulationEx();
+		obj.setName("Encapsulation");
+		obj.getName();
+	}
+
+}
