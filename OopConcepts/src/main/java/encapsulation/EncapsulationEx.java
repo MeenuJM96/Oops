7c3d724c3a51a1ab6b78cee2f27398f53bc9766c
hp <hp@LAPTOP-116NDJE9>
2024-05-07 21:49:48 +0530

    Encapsulation

--- OopConcepts/src/main/java/encapsulation/EncapsulationEx.java
+++ OopConcepts/src/main/java/encapsulation/EncapsulationEx.java
@@ -2,10 +2,11 @@ package encapsulation;
 
 public class EncapsulationEx {
 	private String name;
-	public void getName() {
-		System.out.println("Name:"+name);
-	}
+	
 	public void setName(String name) {
 		this.name=name;
+	}
+	public void getName() {
+		System.out.println("Name:"+name);
 	}
 }
